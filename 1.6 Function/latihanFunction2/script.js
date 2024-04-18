40681246755a538693b34268251900a744a55285
Taopik-Nurhidayat <telopick01@gmail.com>
2024-04-18 13:43:42 +0700

    kalkulator J & R

--- 1.6 Function/latihanFunction2/script.js
+++ 1.6 Function/latihanFunction2/script.js
@@ -1,106 +1,106 @@
-//Kalkulator Bangun Ruang
-function kubus(a) {
-  return a ** 3;
-}
+// //Kalkulator Bangun Ruang
+// function kubus(a) {
+//   return a ** 3;
+// }
 
-function balok(a, b, c) {
-  return a * b * c;
-}
+// function balok(a, b, c) {
+//   return a * b * c;
+// }
 
-function tabung(a, b) {
-  return (22 / 7) * a ** 2 * b;
-}
+// function tabung(a, b) {
+//   return (22 / 7) * a ** 2 * b;
+// }
 
-function bola(a) {
-  return (((4 / 3) * 22) / 7) * a ** 3;
-}
-function Kerucut(a, b) {
-  return (1 / 3) * (22 / 7) * a ** 2 * b;
-}
+// function bola(a) {
+//   return (((4 / 3) * 22) / 7) * a ** 3;
+// }
+// function Kerucut(a, b) {
+//   return (1 / 3) * (22 / 7) * a ** 2 * b;
+// }
 
-let lanjut;
-do {
-  console.log("--KALKULATOR VOLUME BANGUN RUANG--");
-  console.log("=================================");
-  console.log("--Silahkan pilih menu di bawah: ");
-  console.log("--1. Kubus (panjang sisi^3)");
-  console.log("--2. Balok (p * l * t)");
-  console.log("--3. Tabung (π * r^2 * t)");
-  console.log("--4. Bola ( 4/3 * π * r^3 )");
-  console.log("--5. Kerucut (1/3 π * r^2 * t )");
-  console.log("=================================");
+// let lanjut;
+// do {
+//   console.log("--KALKULATOR VOLUME BANGUN RUANG--");
+//   console.log("=================================");
+//   console.log("--Silahkan pilih menu di bawah: ");
+//   console.log("--1. Kubus (panjang sisi^3)");
+//   console.log("--2. Balok (p * l * t)");
+//   console.log("--3. Tabung (π * r^2 * t)");
+//   console.log("--4. Bola ( 4/3 * π * r^3 )");
+//   console.log("--5. Kerucut (1/3 π * r^2 * t )");
+//   console.log("=================================");
 
-  const menu = prompt("Pilih Menu: ");
-  let a, b;
+//   const menu = prompt("Pilih Menu: ");
+//   let a, b;
 
-  switch (menu) {
-    case "1":
-      a = parseInt(prompt("Masukan panjang sisi kubus :"));
-      console.log("Volume kubus dari " + a + "^3 " + "= " + kubus(a));
-      break;
+//   switch (menu) {
+//     case "1":
+//       a = parseInt(prompt("Masukan panjang sisi kubus :"));
+//       console.log("Volume kubus dari " + a + "^3 " + "= " + kubus(a));
+//       break;
 
-    case "2":
-      a = prompt("Masukan panjang balok :");
-      b = prompt("Masukan lebar balok :");
-      c = prompt("Masukan tinggi balok :");
-      console.log(
-        "Volume balok dari " +
-          a +
-          "*" +
-          b +
-          "*" +
-          c +
-          " = " +
-          parseInt(balok(a, b, c))
-      );
-      break;
+//     case "2":
+//       a = prompt("Masukan panjang balok :");
+//       b = prompt("Masukan lebar balok :");
+//       c = prompt("Masukan tinggi balok :");
+//       console.log(
+//         "Volume balok dari " +
+//           a +
+//           "*" +
+//           b +
+//           "*" +
+//           c +
+//           " = " +
+//           parseInt(balok(a, b, c))
+//       );
+//       break;
 
-    case "3":
-      a = prompt("Masukan Jari-jari alas tabung :");
-      b = prompt("Masukan Tinggi tabung :");
-      console.log(
-        "Volume tabung dari " +
-          "22/7 * " +
-          a +
-          "^2" +
-          " " +
-          "* " +
-          b +
-          " = " +
-          parseInt(tabung(a, b))
-      );
-      break;
+//     case "3":
+//       a = prompt("Masukan Jari-jari alas tabung :");
+//       b = prompt("Masukan Tinggi tabung :");
+//       console.log(
+//         "Volume tabung dari " +
+//           "22/7 * " +
+//           a +
+//           "^2" +
+//           " " +
+//           "* " +
+//           b +
+//           " = " +
+//           parseInt(tabung(a, b))
+//       );
+//       break;
 
-    case "4":
-      a = prompt("Masukan Jari-jari bola :");
-      console.log(
-        "Volume bola dari " +
-          "4/3 * 22/7 * " +
-          a +
-          "^3" +
-          " = " +
-          parseFloat(bola(a))
-      );
-      break;
+//     case "4":
+//       a = prompt("Masukan Jari-jari bola :");
+//       console.log(
+//         "Volume bola dari " +
+//           "4/3 * 22/7 * " +
+//           a +
+//           "^3" +
+//           " = " +
+//           parseFloat(bola(a))
+//       );
+//       break;
 
-    case "5":
-      a = prompt("Masukan Jari-jari alas :");
-      b = prompt("Masukan tinggi kerucut :");
-      console.log(
-        "Volume kerucut dari  1/3 * 22/7 * " +
-          a +
-          "^2" +
-          " * " +
-          b +
-          " = " +
-          parseFloat(bola(a, b))
-      );
-      break;
-    default:
-      alert("Menu yang anda pilih tidak tersedia!");
-      break;
-  }
-  lanjut = prompt("Mau pilih menu yang lain? (Y/T)");
-} while (lanjut === "Y" || lanjut === "y");
+//     case "5":
+//       a = prompt("Masukan Jari-jari alas :");
+//       b = prompt("Masukan tinggi kerucut :");
+//       console.log(
+//         "Volume kerucut dari  1/3 * 22/7 * " +
+//           a +
+//           "^2" +
+//           " * " +
+//           b +
+//           " = " +
+//           parseFloat(bola(a, b))
+//       );
+//       break;
+//     default:
+//       alert("Menu yang anda pilih tidak tersedia!");
+//       break;
+//   }
+//   lanjut = prompt("Mau pilih menu yang lain? (Y/T)");
+// } while (lanjut === "Y" || lanjut === "y");
 
-console.log("--PROGRAM SELESAI--");
+// console.log("--PROGRAM SELESAI--");
---
+++ 1.6 Function/latihanFunction2/script2.js
@@ -0,0 +1,37 @@
+//jumlah
+function hitungJml(arr) {
+  jumlah = 0;
+  for (let i = 0; i < arr.length; i++) {
+    jumlah = jumlah + arr[i];
+  }
+  return jumlah;
+}
+
+//hitung rata-rata
+function hitungRata(arr) {
+  if (arr.length === 0) return;
+  return hitungJml(arr) / arr.length;
+}
+
+//inputan user dan membuat array
+function buatArray() {
+  const jumlahElm = parseInt(prompt("Masukan jumlah element pada array: "));
+
+  let arr = [];
+  for (let i = 0; i < jumlahElm; i++) {
+    arr.push(parseInt(prompt("Masukkan angka ke-" + (i + 1) + ":")));
+  }
+  return arr;
+}
+
+const inputArray = buatArray();
+
+//masukan nilai jumlah dan rata-rata
+
+const jml = hitungJml(inputArray);
+const rta = hitungRata(inputArray);
+
+//menampilkan
+console.log("Array yang dimasukkan:", inputArray);
+console.log("Jumlah dari elemen-elemen dalam array:", jml);
+console.log("Rata-rata dari elemen-elemen dalam array:", rta);
